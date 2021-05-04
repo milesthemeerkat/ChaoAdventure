e9d5b1a8459974118e86609be077a61a29550cc8
Miles <milesthemeerkat@gmail.com>
2021-05-03 23:18:11 -0700

    Adding boilerplate unit tests

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+import unittest
+from chao_adventure.main import main
+
+class TestMain(unittest.TestCase):
+    def test_main(self):
+        """
+        Test that main() returns Hello, World!
+        """
+        self.assertEqual("Hello, World!", main())
